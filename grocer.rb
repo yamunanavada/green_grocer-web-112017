eca09e92e245d9bf3b1ba14727b09b0978dc5364
Yamuna Navada <yamunanavada@gmail.com>
2017-11-08 15:07:56 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -54,7 +54,7 @@ def checkout(cart: [], coupons: [])
   # code here
   total = 0
   cart = consolidate_cart(cart)
-  
+
   if cart.length == 1
     cart = apply_coupons(cart, coupons)
     cart_clearance = apply_clearance(cart)
@@ -76,7 +76,7 @@ def checkout(cart: [], coupons: [])
       total += (details[:price]*details[:count])
     end
   end
-  
+
 
   if total > 100
     total = total*(0.90)
